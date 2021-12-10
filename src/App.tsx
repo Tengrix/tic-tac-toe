5b10975e2c3288a20a40fab94e3fabe9ec7b038e
Tengrix <damir861@gmail.com>
2021-12-10 00:05:47 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [ticTacToe,setTicTacToe] = useState<number>(9)
+  const [state, setState] = useState<Array<number>>([])
+  useEffect(()=>{
+    for(let i = 0; i < ticTacToe; i++){
+      state.push(i)
+    }
+    setState([...state])
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.map(el=> <div className={'square'}>{el}</div>)}
     </div>
   );
 }
