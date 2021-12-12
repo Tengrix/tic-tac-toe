87e5bdfec0b922d5383cf9c4e4ea32683419eaa5
Tengrix <damir861@gmail.com>
2021-12-12 01:38:29 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
+type initialStateType = {
+    id: number;
+    value: string | null
+}
+const initialState = Array(9).fill({}).map(el => ({id: Math.random() * 10, value: null}))
+
 function App() {
-  const [ticTacToe,setTicTacToe] = useState<number>(9)
-  const [state, setState] = useState<Array<number>>([])
-  useEffect(()=>{
-    for(let i = 0; i < ticTacToe; i++){
-      state.push(i)
+    const [state, setState] = useState<initialStateType[]>(initialState)
+    const [turn, setTurn] = useState<boolean>(false)
+    const [comboOne, setComboOne] = useState<Array<number>>([])
+    const [comboTwo, setComboTwo] = useState<Array<number>>([])
+
+    const onClick = (id: number) => {
+        setTurn(!turn)
+        const newState = state.map(el => el.id === id ? {...el, value: turn ? 'X' : 'O'} : el)
+        setState(newState)
+        if (!turn) {
+            state.map((el, i) => el.id === id ? comboOne.push(i + 1) : '')
+            setComboOne([...comboOne])
+        }
     }
-    setState([...state])
-  },[])
-  return (
-    <div className="App">
-      {state.map(el=> <div className={'square'}>{el}</div>)}
-    </div>
-  );
+    const winningCombo = [
+        [1, 2, 3],
+        [1, 4, 7],
+        [1, 5, 9],
+        [2, 5, 8],
+        [3, 5, 7],
+        [4, 5, 6],
+        [7, 8, 9],
+        [3, 6, 9],
+
+    ]
+    useEffect(() => {
+        console.log('damir')
+    }, [comboOne])
+    console.log(comboOne)
+    return (
+        <div className="App">
+            {state.map((el, i) => <div key={i} onClick={() => onClick(el.id)} className={'square'}>{el.value}</div>)}
+            <div>
+                turn:{!turn ? 'Player 1' : 'Player 2'}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
