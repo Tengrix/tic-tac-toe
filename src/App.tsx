0a6a338bd60eff312d6a1516748eccec5b455fbd
Tengrix <damir861@gmail.com>
2021-12-13 00:08:23 -0500

    start again

--- src/App.tsx
+++ src/App.tsx
@@ -10,40 +10,45 @@ const initialState = Array(9).fill({}).map(el => ({id: Math.random() * 10, value
 function App() {
     const [state, setState] = useState<initialStateType[]>(initialState)
     const [turn, setTurn] = useState<boolean>(false)
-    const [comboOne, setComboOne] = useState<Array<number>>([])
-    const [comboTwo, setComboTwo] = useState<Array<number>>([])
+    const [steps, setSteps] = useState<number>(0)
 
     const onClick = (id: number) => {
         setTurn(!turn)
         const newState = state.map(el => el.id === id ? {...el, value: turn ? 'X' : 'O'} : el)
         setState(newState)
-        if (!turn) {
-            state.map((el, i) => el.id === id ? comboOne.push(i + 1) : '')
-            setComboOne([...comboOne])
-        }
+        setSteps(steps+1)
     }
-    const winningCombo = [
-        [1, 2, 3],
-        [1, 4, 7],
-        [1, 5, 9],
-        [2, 5, 8],
-        [3, 5, 7],
-        [4, 5, 6],
-        [7, 8, 9],
-        [3, 6, 9],
+    const winnerCheck = () => {
+        const winningCombo = [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8],
+            [2, 5, 8],
 
-    ]
-    useEffect(() => {
-        console.log('damir')
-    }, [comboOne])
-    console.log(comboOne)
+        ]
+        for(let i = 0; i < winningCombo.length; i++){
+            let [a,b,c] = winningCombo[i]
+            if(state[a].value===state[b].value && state[b].value===state[c].value) return state[a].value
+        }
+        return null
+    }
+    const startAgain = () => {
+        setState(initialState)
+        setTurn(false)
+    }
+    const winner = winnerCheck()
+    console.log(steps)
     return (
         <div className="App">
             {state.map((el, i) => <div key={i} onClick={() => onClick(el.id)} className={'square'}>{el.value}</div>)}
             <div>
-                turn:{!turn ? 'Player 1' : 'Player 2'}
+                {winner===null? <p>Next: {!turn?'Player 1':'Player 2'}</p> : steps===9 ?<p>Winner: {'Friendship'}</p>:<p>Winner: {winner}</p>  }
+                {winner !== null && <button onClick={startAgain}>start again</button>}
             </div>
-
         </div>
     );
 }
